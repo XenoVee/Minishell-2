abfbc0d3f8a59832ec1b562dee491f4e01ea8c1c
Rosa Maes <rmaes@student.codam.nl>
2024-09-19 18:14:02 +0200

    good girl

--- sources/input/input_parse.c
+++ sources/input/input_parse.c
@@ -6,7 +6,7 @@
 /*   By: rmaes <rmaes@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/08 19:08:33 by rmaes         #+#    #+#                 */
-/*   Updated: 2024/09/16 18:13:51 by rmaes         ########   odam.nl         */
+/*   Updated: 2024/09/18 16:49:25 by rmaes         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,30 +38,44 @@ static void	func_cmd(t_comm_data *c_data, t_dllist *lex, t_dllist *env)
 	c_data->cmd[i] = NULL;
 }
 
-static t_comm_data	*parser(char **array, t_comm_data *c_data, t_dllist *env)
+static t_comm_data	*run_lexer(char **array, t_comm_data *c_data, t_dllist *env)
 {
 	t_dllist	*lex;
 
-	c_data->fd_input = 0;
-	c_data->fd_output = 0;
-	if (array[1] != NULL)
-	{
-		c_data->next = malloc(sizeof(t_comm_data) + 1);
-		c_data->next->prev = c_data;
-		if (parser(&array[1], c_data->next, env) == NULL)
-			return (NULL);
-	}
-	else
-		c_data->next = NULL;
 	lex = lexer(array[0]);
 	func_cmd(c_data, lex, env);
 	if (func_infiles(c_data, lex, env) || func_outfiles(c_data, lex, env) == -1)
 	{
 		perror("Minishell");
+		free (c_data->cmd);
+		if (c_data->next)
+			free (c_data->next);
+		cdl_listclear(lex);
 		return (NULL);
 	}
 	cdl_listclear(lex);
 	return (c_data);
+}
+
+static t_comm_data	*parser(char **array, t_comm_data *c_data, t_dllist *env)
+{
+	c_data->fd_input = 0;
+	c_data->fd_output = 0;
+	if (array[1] != NULL)
+	{
+		c_data->next = malloc(sizeof(t_comm_data) + 1);
+		if (c_data->next == NULL)
+			return (NULL);
+		c_data->next->prev = c_data;
+		if (parser(&array[1], c_data->next, env) == NULL)
+		{
+			free (c_data->next);
+			return (NULL);
+		}
+	}
+	else
+		c_data->next = NULL;
+	return (run_lexer(array, c_data, env));
 }
 
 t_comm_data	*input_parse(char *input, t_dllist *env)
